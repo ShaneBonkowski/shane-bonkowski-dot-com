7859a8912c63a2853269deeb16cbdade21a6c69f
ShaneBonkowski <114165363+ShaneBonkowski@users.noreply.github.com>
2025-05-08 12:42:21 -0700

    Added more TODO's

--- src/games/better-boids/BoidsGameComponent.tsx
+++ src/games/better-boids/BoidsGameComponent.tsx
@@ -16,10 +16,14 @@ interface BoidsGameComponentProps {
 //   This leads to an issue where canvas's would be duplicated. I fixed the duplication,
 //   but I'm still getting some weird behavior with phaser scale etc. not being init'd on
 //   a hot reload. To test, go to menu and come back to the app quickly, it will
-//   hot reload and cause issues.
+//   hot reload and cause issues... I think the bug is because the resize observer is not cleanly shut down and told to stop listening.
+//   Might be related to the fact that like 3 different listeners exist for the resize.. maybe we should consolidate or at least make sure they all shut down.
 // - Clean up the info button.
 // - Make it so that x button and styling on the info screen is consistent with rest of the website coloring sizing etc.
 // - Game screen should have a banner that is readable. Consider if we should be able to toggle light mode here.. Maybe we have a custom header for games with just shanes games in top left. Then coloring is specific color with no dark/light mode
+// - Need to find a way to generalize the loading behavior (or at least some of the complex parts of it) for the game components. Right now there is a lot of copy-paste when I make fixes.
+// - Make sure all subscribe events are cleanly shut down! Every subscribe should have an unsubscribe. Also, search for all even listeners and make sure they eventually stop listening when shut down.
+//   Also, make sure shutdown logic for game objs etc. is intuitive. We need to guarentee that if someone adds an "unsubsribe" that it will get called automatically, or it is at least clear when they need to call it... such as the for loop to shut down boids.
 
 // Singleton Phaser game instance
 let game: Phaser.Game | null = null;
