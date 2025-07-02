c5fc21deda4dd0731200b880398ef6fc7e974c65
ShaneBonkowski <sbonkows@terpmail.umd.edu>
2025-07-02 00:09:25 -0700

    Fixed it so that it actually assigns top and bottom properly

--- src/components/StoryParagraphGroup.tsx
+++ src/components/StoryParagraphGroup.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import React from "react";
-import StoryParagraphElement, {
-  StoryParagraphElementProps,
-} from "@/src/components/StoryParagraphElement";
+import { StoryParagraphElementProps } from "@/src/components/StoryParagraphElement";
 
 /**
  * A wrapper component that groups multiple StoryParagraphElement components
@@ -14,19 +12,13 @@ const StoryParagraphGroup = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       {childArray.map((child, idx) => {
-        if (
-          React.isValidElement(child) &&
-          child.type === StoryParagraphElement
-        ) {
-          return React.cloneElement(
-            child as React.ReactElement<StoryParagraphElementProps>,
-            {
-              isFirst: idx === 0,
-              isLast: idx === childArray.length - 1,
-            }
-          );
-        }
-        return child;
+        return React.cloneElement(
+          child as React.ReactElement<StoryParagraphElementProps>,
+          {
+            isFirst: idx === 0,
+            isLast: idx === childArray.length - 1,
+          }
+        );
       })}
     </>
   );
