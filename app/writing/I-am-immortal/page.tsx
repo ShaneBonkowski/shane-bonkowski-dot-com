a5f7b5e08a62e19ff361e716c6ec6181547ffbac
ShaneBonkowski <sbonkows@terpmail.umd.edu>
2025-07-20 15:29:00 -0700

    I am immortal revision

--- app/writing/I-am-immortal/page.tsx
+++ app/writing/I-am-immortal/page.tsx
@@ -63,8 +63,8 @@ export default function Page() {
             When I was less coalesced, I was a mess of bits and bobs, buzzing
             around the molten slurry that this explosion of existence left in
             its wake. It beckoned me, like a distant mirage amid the endless sea
-            of sand in the Sahara. Or maybe the faint glow of the River Styx
-            guiding this lost soul to paradise. And so, I followed it.
+            of sand in the Sahara. Or maybe like the faint glow of the River
+            Styx guiding this lost soul to paradise. And so, I followed it.
           </WrittenContentParagraphElement>
 
           <WrittenContentParagraphElement
@@ -82,12 +82,12 @@ export default function Page() {
             fontStyle="normal"
             textAlign="justify"
           >
-            It&apos;s hard to piece together exactly how I got here, but somehow
-            somewhere along the way I found myself here in this blue utopia.
-            Amidst the delicate cosmic dance of this Solar System, heavenly
-            bodies crashing together to the symphony of orderly chaos, here I
-            lie among the rubble with the opportunity to bear witness to its
-            greatness.
+            It&apos;s difficult to piece together exactly how I got here, but
+            somehow, somewhere along the way, I found myself here in this blue
+            utopia. Amidst the delicate cosmic dance of this Solar System,
+            heavenly bodies crashing together to the symphony of orderly chaos,
+            here I lie among the rubble with the opportunity to bear witness to
+            its greatness.
           </WrittenContentParagraphElement>
 
           <WrittenContentParagraphElement
@@ -98,8 +98,8 @@ export default function Page() {
             I&apos;ve rained down from the heavens with the hellfire of molten
             rock. I&apos;ve raged with the great rivers, tearing rifts through
             continents. I&apos;ve breathed life into the atmosphere with my
-            mighty lungs of creation. I&apos;ve played my part in this ensemble,
-            and shaped this planet into what it is.
+            mighty lungs of creation. I&apos;ve played my part in this ensemble
+            and helped shape this planet.
           </WrittenContentParagraphElement>
 
           <WrittenContentParagraphElement fontStyle="normal" textAlign="center">
@@ -110,43 +110,33 @@ export default function Page() {
             fontStyle="normal"
             textAlign="justify"
           >
-            I&apos;ve come to realize I abhor the word <em>Death</em>. Who are
-            you to deny our greatness with that term? <em>Death</em>. You and I,
-            we&apos;re the lucky ones. We lucky few, through sheer determination
-            and the right string of collisions, find ourselves with the gift of{" "}
-            <em>Life</em>. The most precious stage of all cosmic
+            I&apos;ve come to realize that I abhor the word <em>Death</em>. Who
+            are you to deny our greatness with that term? <em>Death</em>. You
+            and I, we&apos;re the lucky ones. We lucky few, through sheer
+            determination and the right string of collisions, find ourselves
+            with this beautiful gift. The most precious stage of all cosmic
             transformations. We get the opportunity to think, to speak, to
             write, to love deeply, to witness it all.
           </WrittenContentParagraphElement>
 
           <WrittenContentParagraphElement fontStyle="normal" textAlign="center">
-            And as much as I hate to say it, to <em>die</em>.
+            And as much as I hate to say it, to die.
           </WrittenContentParagraphElement>
 
           <WrittenContentParagraphElement
             fontStyle="normal"
             textAlign="justify"
           >
-            <em>Death</em> as I see it is not the end, but a new beginning. A
-            chance to return to the order from which we were granted the
-            opportunity for <em>life</em> in the first place. Maybe we will live
-            again. This time a much smaller part of some much smaller,
-            insignificant species. Or perhaps something far greater. All just a
-            break for a weary traveler on their journey through the Infinity.
-          </WrittenContentParagraphElement>
-
-          <WrittenContentParagraphElement
-            fontStyle="normal"
-            textAlign="justify"
-          >
-            I&apos;ve always found myself running towards something. Something
-            growing infinitely further. And yet, just out of reach. Will I ever
-            reach it? Will <em>we</em> ever reach it? I hope so. At least we
-            have forever to find out.
+            <em>Death,</em> as I see it, is not the end, but a new beginning. A
+            chance to return to the order from which we were granted this
+            opportunity in the first place. Maybe we will live again. This time,
+            a much smaller part of some much smaller, insignificant species. Or
+            perhaps something far greater. All just a break for a weary traveler
+            on their journey through the Infinity.
           </WrittenContentParagraphElement>
 
           <WrittenContentParagraphElement fontStyle="normal" textAlign="center">
-            If you ask me, I am immortal and so are you.
+            If you ask me, I am immortal, and so are you.
           </WrittenContentParagraphElement>
         </WrittenContentParagraphGroup>
       </WrittenContentLoader>
