06bf43e129b53df206017f2de9119ca481cdeed8
ShaneBonkowski <sbonkows@terpmail.umd.edu>
2025-10-26 13:11:40 -0700

    Save the comic being viewed to the url

--- src/components/ComicContentLoader.tsx
+++ src/components/ComicContentLoader.tsx
@@ -14,6 +14,7 @@ import {
   FaForward,
   FaStepForward,
 } from "react-icons/fa";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const random: SeededRandom = new SeededRandom(randomType.UNSEEDED_RANDOM);
 
@@ -67,6 +68,10 @@ const ComicContentLoader: React.FC<ComicContentLoaderProps> = ({
     [childrenComicsData]
   );
 
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const urlComicNum = searchParams.get("comic");
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const [initialized, setInitialized] = useState(false);
 
@@ -80,35 +85,52 @@ const ComicContentLoader: React.FC<ComicContentLoaderProps> = ({
     if (initialized) return;
     if (sortedComics.length === 0) return;
 
-    const firstComicNum = Math.min(
-      ...childrenComicsData.map((c) => c.comicNum)
-    );
-    /* eslint-disable no-restricted-syntax */
-    const seenFirstComic =
-      localStorage.getItem(`${comicData.title}-${firstComicNum}-seen`) ==
-      "true";
-    /* eslint-enable no-restricted-syntax */
-
     // Decide initial index of comic to show user at first
     let newIndex = 0;
 
-    switch (comicData.firstComicShown) {
-      case "FIRST":
-        newIndex = 0;
-        break;
-      case "LAST":
-        newIndex = sortedComics.length - 1;
-        break;
-      case "FIRST_IF_NOT_SEEN_THEN_LAST":
-        // If user has seen the first already, then default to showing the last
-        // (most recent), since it assumes that means the user is ready to see the
-        // most recent one.
-        newIndex = seenFirstComic ? sortedComics.length - 1 : 0;
-        break;
-      default:
-        console.error(
-          `Unexpected firstComicShown value: ${comicData.firstComicShown}`
-        );
+    // If URL contains ?comic=, prefer that. So that users can send eachother links to exact comics.
+    if (urlComicNum) {
+      console.log("Using '?comic=' to determine which comic to view");
+
+      const urlIndex = sortedComics.findIndex(
+        (c) => c.comicNum === Number(urlComicNum)
+      );
+      if (urlIndex >= 0) {
+        newIndex = urlIndex;
+      }
+    } else {
+      // Otherwise, determine it based on user local storage and the comic preferences
+      console.log(
+        "Using local storage + comic preferences to determine which comic to view"
+      );
+
+      const firstComicNum = Math.min(
+        ...childrenComicsData.map((c) => c.comicNum)
+      );
+      /* eslint-disable no-restricted-syntax */
+      const seenFirstComic =
+        localStorage.getItem(`${comicData.title}-${firstComicNum}-seen`) ==
+        "true";
+      /* eslint-enable no-restricted-syntax */
+
+      switch (comicData.firstComicShown) {
+        case "FIRST":
+          newIndex = 0;
+          break;
+        case "LAST":
+          newIndex = sortedComics.length - 1;
+          break;
+        case "FIRST_IF_NOT_SEEN_THEN_LAST":
+          // If user has seen the first already, then default to showing the last
+          // (most recent), since it assumes that means the user is ready to see the
+          // most recent one.
+          newIndex = seenFirstComic ? sortedComics.length - 1 : 0;
+          break;
+        default:
+          console.error(
+            `Unexpected firstComicShown value: ${comicData.firstComicShown}`
+          );
+      }
     }
 
     setCurrentIndex(newIndex);
@@ -117,8 +139,9 @@ const ComicContentLoader: React.FC<ComicContentLoaderProps> = ({
     comicData.firstComicShown,
     comicData.title,
     childrenComicsData,
-    sortedComics.length,
+    sortedComics,
     initialized,
+    urlComicNum,
   ]);
 
   useEffect(() => {
@@ -127,16 +150,23 @@ const ComicContentLoader: React.FC<ComicContentLoaderProps> = ({
     // rendered environment.
     if (typeof window === "undefined") return;
 
-    // Only update "mark comic as seen" effect after initialization.
+    // Only update "mark comic as seen" effect after initialization. This will
+    // save to local storage.
     if (!initialized || currentIndex == null) return;
 
     const currentComicNum = sortedComics[currentIndex]?.comicNum;
     if (currentComicNum == null) return;
 
-    /* eslint-disable no-restricted-syntax */
+    // eslint-disable-next-line no-restricted-syntax
     localStorage.setItem(`${comicData.title}-${currentComicNum}-seen`, "true");
-    /* eslint-enable no-restricted-syntax */
-  }, [currentIndex, sortedComics, comicData.title, initialized]);
+
+    // Update the url to reflect the current comic shown
+    // eslint-disable-next-line no-restricted-syntax
+    const currentParams = new URLSearchParams(window.location.search);
+    if (currentParams.get("comic") !== String(currentComicNum)) {
+      router.replace(`?comic=${currentComicNum}`, { scroll: false });
+    }
+  }, [currentIndex, sortedComics, comicData.title, initialized, router]);
 
   const currentChildComicData = useMemo(
     () => sortedComics[currentIndex],
