c3e890aa6a7e884bcc824388b93fb6ce05ff4fd7
ShaneBonkowski <sbonkows@terpmail.umd.edu>
2025-10-26 13:33:53 -0700

    Implemented async loading of comics via 'suspense' so that we dont need to statically generate the page(s)

--- app/comics/southward-falls/page.tsx
+++ app/comics/southward-falls/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import ComicContentLoader from "@/src/components/ComicContentLoader";
 import { ComicMetadataProps } from "@/src/types/data-props";
 import { southwardFallsComicData } from "@/src/data/comic-data";
@@ -37,11 +38,25 @@ export const metadata = {
   },
 };
 
-export default function Page() {
+export default async function Page() {
   return (
-    <ComicContentLoader
-      comicData={comicData}
-      childrenComicsData={southwardFallsComicData}
-    />
+    // Use 'Suspense' here since this is a client-side rendered page. We do this
+    // because there is no one comic tied to the comic series. It changes based on
+    // which comic the user is viewing etc. No fallback UI is used because I would
+    // prefer it to just show the home page for longer rather than a few milliseconds
+    // of a loading screen
+    <Suspense
+      key={comicData.title}
+      fallback={
+        <div className="flex items-center justify-center h-full my-auto">
+          <h3>Loading comic content...</h3>
+        </div>
+      }
+    >
+      <ComicContentLoader
+        comicData={comicData}
+        childrenComicsData={southwardFallsComicData}
+      />
+    </Suspense>
   );
 }
